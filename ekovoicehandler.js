cbb185f3bade53055fc706fee034070963fee222
kevin_pascale <kevin.pascale.97@outlook.it>
2019-11-10 15:46:06 +0100

    first commit

---
+++ ekovoicehandler.js
@@ -0,0 +1,91 @@
+//ekovoicehandler library
+
+  import Artyom from "artyom.js";
+
+  function onInit(player, language, actTime){ //eko player, string language and command activation time boolean as parameters
+
+
+  const voiceHandler = new Artyom(); //create artyom object
+
+  function startRec(lang){   //string & boolean parameters
+
+    console.log("activating vocal commands");
+    voiceHandler.fatality();  //stop any other recognition
+
+    setTimeout(function(){    // if you use artyom.fatality , wait 250 ms to initialize again.
+      voiceHandler.initialize({
+          lang:lang,
+          continuous: true,
+          listen:true,
+          debug:true,
+          speed:1
+       }).then(function(){
+        console.log("VoiceHandler activated!");
+      });
+  },250);
+
+  }
+
+  function createCommand(indexes, idNodo){ //string array + string id nodo o idBottone? o altra proprietà del nodo?? DEVO AGGIUNGERE IL PLAYER???
+
+    var command = {
+      indexes: indexes,// array of words, will trigger the execution of the command
+      action:function() { // Action to be executed when a index match with spoken word
+
+        player.seek(idNodo); //append the node to the playlist
+
+    }
+  };
+    voiceHandler.addCommands(command);
+}
+
+  function getCurrentNodeChildren(){ //gets currentNode children node array
+
+    var decisionInstance = player.decision.get(player.currentNodeId);
+    var childrenNodes = decisionInstance.children;
+    return childrenNodes;
+
+  }
+
+  function addVocalCommands(){
+
+   var nodi = player.repository.get(getCurrentNodeChildren()); //oppure getnodechildren con parametro nodo corrente
+   var keywords = null;
+   var nodofiglio = null;
+   for(var i = 0;i < nodi.length; i++){
+
+     console.log(nodi[i]);
+     console.log("idnodofiglio"+i+":",nodi[i].data.id);
+     keywords = nodi[i].data.studio.keywords;
+     nodofiglio = nodi[i];
+
+     if(keywords != undefined) createCommand(keywords,nodofiglio);
+
+   }
+  }
+
+
+  startRec(language);
+  switch (actTime) {
+    case "decision":
+      player.on('nodestart',function(){voiceHandler.dontObey();});
+      player.on('decision.start',function(){
+        voiceHandler.obey();
+        voiceHandler.emptyCommands();
+        addVocalCommands();
+      });
+      break;
+    case "always":
+      player.on('nodestart',function(){
+        voiceHandler.emptyCommands();
+        addVocalCommands();
+      });
+      break;
+    default:
+      console.log("you didn't specified the commands' activation time");
+      break;
+  }
+
+  }
+
+  export default { onInit:onInit };
